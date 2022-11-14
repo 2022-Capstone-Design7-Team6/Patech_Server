561b9466ce36e9a4fe18b7dec77b97cf191951ab
HunSeokJeong <44901486+HunSeokJeong@users.noreply.github.com>
2022-11-14 18:58:57 +0900

    [feat] admin page 추가

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
-
+from gallery.models import Price,Plant,Photo
 class ProfileInline(admin.StackedInline):
     model=Profile
     can_delete =True
@@ -11,6 +11,12 @@ class ProfileInline(admin.StackedInline):
 
 class UserAdmin(BaseUserAdmin):
     inlines =(ProfileInline,)
+
+    
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
+admin.site.register(Price)
+admin.site.register(Plant)
+admin.site.register(Photo)
+
 # Register your models here.
